0e3858ed03db4b69887eb05aa8540e0f1eef8555
Emily Shen <shenyit@seas.upenn.edu>
2025-03-17 23:14:29 -0400

    checking types

--- dcgmReaderToInfluxDB.py
+++ dcgmReaderToInfluxDB.py
@@ -217,6 +217,7 @@ def DcgmReaderDictionary(hostname, field_ids, update_frequency, keep_time, ignor
                         except (ValueError, TypeError):
                             # otherwise, keep as string (should all be nums though)
                             point.field(fieldName, str(latest_value))
+                        print(type(latest_value))
 
                 # Compute FB_UTIL (Framebuffer Utilization)
                 fb_used = gpuData.get("fb_used")
